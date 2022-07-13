70e850689c93ed5e33b9432861e205a7044bbd90
Alexander <dr.ambercat@gmail.com>
2022-07-14 01:17:39 +0300

    first commit

---
+++ public/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>Automatic Deploy</h1>
+      <div className="note">production</div>
+    </div>
+  )
+}
